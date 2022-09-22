56f798998428f9b6783446b810ec90dc3a84daa7
yiladien@hotmail.com <yiladien@hotmail.com>
2022-09-21 21:37:41 -0500

    refactoring capture

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const generateHTML = require("./src/generatePage");
 
+const managerPrompt = () => {
+  return;
+};
 const initialQuestions = [
   {
     type: "input",
@@ -16,99 +19,116 @@ const initialQuestions = [
       }
     },
   },
-  //   {
-  //     type: "input",
-  //     name: "employeeID",
-  //     message: "Please provide the manager's employee ID:",
-  //     validate: (employeeIDInput) => {
-  //       if (employeeIDInput) {
-  //         return true;
-  //       } else {
-  //         console.log("Please enter an employee ID!");
-  //         return false;
-  //       }
-  //     },
-  //   },
-  //   {
-  //     type: "input",
-  //     name: "email",
-  //     message: "Please provide the manager's email address:",
-  //     validate: (emailInput) => {
-  //       if (emailInput) {
-  //         return true;
-  //       } else {
-  //         console.log("Must provide an email address!");
-  //         return false;
-  //       }
-  //     },
-  //   },
-  //   {
-  //     type: "input",
-  //     name: "officeNumber",
-  //     message: "What is the office number?",
-  //     validate: (officeNumberInput) => {
-  //       if (officeNumberInput) {
-  //         return true;
-  //       } else {
-  //         console.log("Please provide the manager's office number!");
-  //         return false;
-  //       }
-  //     },
-  //   },
+  // {
+  //   type: "list",
+  //   name: "employeeAdd",
+  //   message: "How would you like to proceed?",
+  //   choices: ["ADD Engineer", "ADD Intern", "Build team page"],
+  //   loop: true,
+  //   when: ({ employeeAdd.loop  }) => employeeAdd,
+  // },
 ];
+//   {
+//     type: "input",
+//     name: "employeeID",
+//     message: "Please provide the manager's employee ID:",
+//     validate: (employeeIDInput) => {
+//       if (employeeIDInput) {
+//         return true;
+//       } else {
+//         console.log("Please enter an employee ID!");
+//         return false;
+//       }
+//     },
+//   },
+//   {
+//     type: "input",
+//     name: "email",
+//     message: "Please provide the manager's email address:",
+//     validate: (emailInput) => {
+//       if (emailInput) {
+//         return true;
+//       } else {
+//         console.log("Must provide an email address!");
+//         return false;
+//       }
+//     },
+//   },
+//   {
+//     type: "input",
+//     name: "officeNumber",
+//     message: "What is the office number?",
+//     validate: (officeNumberInput) => {
+//       if (officeNumberInput) {
+//         return true;
+//       } else {
+//         console.log("Please provide the manager's office number!");
+//         return false;
+//       }
+//     },
+//   },
 
-const employeeInput = [
-  {
-    type: "input",
-    name: "employeeID",
-    message: "Please provide the manager's employee ID:",
-    validate: (employeeIDInput) => {
-      if (employeeIDInput) {
-        return true;
-      } else {
-        console.log("Please enter an employee ID!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Please provide the manager's email address:",
-    validate: (emailInput) => {
-      if (emailInput) {
-        return true;
-      } else {
-        console.log("Must provide an email address!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "officeNumber",
-    message: "What is the office number?",
-    validate: (officeNumberInput) => {
-      if (officeNumberInput) {
-        return true;
-      } else {
-        console.log("Please provide the manager's office number!");
-        return false;
-      }
-    },
-  },
-];
+// const employeeInput = [
+//   {
+//     type: "input",
+//     name: "employeeID",
+//     message: "Please provide the manager's employee ID:",
+//     validate: (employeeIDInput) => {
+//       if (employeeIDInput) {
+//         return true;
+//       } else {
+//         console.log("Please enter an employee ID!");
+//         return false;
+//       }
+//     },
+//   },
+//   {
+//     type: "input",
+//     name: "email",
+//     message: "Please provide the manager's email address:",
+//     validate: (emailInput) => {
+//       if (emailInput) {
+//         return true;
+//       } else {
+//         console.log("Must provide an email address!");
+//         return false;
+//       }
+//     },
+//   },
+//   {
+//     type: "input",
+//     name: "officeNumber",
+//     message: "What is the office number?",
+//     validate: (officeNumberInput) => {
+//       if (officeNumberInput) {
+//         return true;
+//       } else {
+//         console.log("Please provide the manager's office number!");
+//         return false;
+//       }
+//     },
+//   },
+// ];
 
 // test
 function init() {
   inquirer
-    .prompt(initialQuestions)
+    .prompt(managerPrompt)
     .then((managerInput) => {
       console.log(managerInput);
+      return employeeInput(managerInput);
     })
-    .prompt(initialQuestions)
-    .then((employeeInput) => {
-      console.log(employeeInput);
+    .then((employeeData) => {
+      // Combine manager and employee data
+
+      // Send to looping function
+      return addTeamMember(teamList);
+    })
+    .then((writeFileResponse) => {
+      console.log(writeFileResponse);
+    })
+    .catch((err) => {
+      console.log(err);
     });
 }
 
